f00808b225f5c15dae57c236da85f3589402f6cb
Paul Kotlyar <unclepaul84@gmail.com>
2017-09-21 21:13:08 -0500

    changed name back

--- appManifest.js
+++ appManifest.js
@@ -253,19 +253,20 @@ A.app({
             title: 'AllFiledsView Limited',
             showInGrid: ['text', 'integer'],
             filtering: { integer: { $gt: 100 } },
-            sorting: [['integer', -1]]
+            sorting: [['integer', -1]],
+            actions: [
+              {
+                id: 'proceed',
+                name: "SetCheckboxFalse",
+                perform: function (Crud, User, Actions, Security) {
+                    console.warn("action!");
+                },
+                actionTarget: 'all-items'
+              }
+            ]
           }
-        },
-        actions: [
-          {
-            id: 'proceed',
-            name: "SetCheckboxFalse",
-            perform: function (Crud, User, Actions, Security) {
-                console.warn("action!");
-            },
-            actionTarget: 'all-items'
-          }
-        ]
+        }
+       
       },
       AllFiledsParent: {
         fields: {
