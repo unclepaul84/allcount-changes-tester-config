41af497ef9970c74dd4b81d5fdeb077781e2f185
Paul Kotlyar <unclepaul84@gmail.com>
2017-10-05 15:48:05 -0500

    testing webhooks

--- appManifest.js
+++ appManifest.js
@@ -55,7 +55,7 @@ A.app({
 
   webHooks: {
 
-    "testwebhook": function (Body, Params, Query, Res, Q) {
+    "testwebhook": function (Crud, User, Body, Params, Query, Res, Q) {
       return Q(Res.json("{'hello':true}"));
     }
   },
